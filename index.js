3d0e2018adbebc5c43dd335e1457703c5e6d84e2
jedzoka <piotrswic2002@gmail.com>
2020-07-24 22:25:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//navigator.serviceWorker.register('sw.js');
+navigator.serviceWorker.register('sw.js');
 var t=document.querySelector("textarea");
 var d=document.querySelectorAll("div");
 var h=document.querySelectorAll("h1");
