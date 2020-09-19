4ac79b76f41a87f5084293fff2c3fa05264d0a18
jedzoka <piotrswic2002@gmail.com>
2020-09-19 22:09:58 +0200

    UX improvements

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 navigator.serviceWorker.register('sw.js');
-var k=[localStorage.getItem(0),,,"https://api.nal.usda.gov/fdc/v1/food"];
+var k=[localStorage.getItem(0),"https://api.nal.usda.gov/fdc/v1/food",];
 var t=document.querySelector("textarea");
 var d=document.querySelectorAll("div");
 var h=document.querySelectorAll("h1");
 var b=document.getElementsByTagName("b");
 var p=document.getElementsByTagName("p");
 var u=window.location.href.split("?");
-var n=new XMLHttpRequest();
 var i=2;
 var e=1;
 var v;
@@ -43,10 +42,7 @@ v=localStorage.getItem(x)
 if(v!==null){
 d[3].innerHTML+="<b id='"+v+"' onclick='a(this.children[0]);c(this.id)' class='translate'><p id='"+x+"' class='notranslate'>0</p> "+v.split(';')[0]+"</b>"}
 else{
-n.open("GET",k[3]+"/"+x+"?api_key="+k[0]);
-n.send();
-n.onload=function(){
-j=JSON.parse(n.responseText);
+fetch(k[1]+"/"+x+"?api_key="+k[0]).then(v=>v.json()).then(j=>{
 if(j.error)w();
 v=j.description;
 if(j.ingredients)v+=" ~ "+j.ingredients;
@@ -57,8 +53,7 @@ while(g<j.length){
 if(j[g].amount)v+=";"+j[g].nutrient.id+";"+j[g].amount;
 if(!localStorage.getItem("a"+j[g].nutrient.id)){localStorage.setItem("a"+j[g].nutrient.id,j[g].nutrient.name.split('Vitamin ')+" "+j[g].nutrient.unitName+";0;0");y("a"+j[g].nutrient.id)}g++}
 localStorage.setItem(x,v)
-if(k[1])location.reload();
-l(x)}}}
+l(x)})}}
 function c(x){g=1;
 v=x.split(";");
 while(g<v.length){
@@ -76,12 +71,11 @@ while(i<j.length){
 j[i].innerHTML=(j[i].innerHTML*e).toFixed(6)*1;
 j[i].title=(j[i].title*e).toFixed(6)*1;
 i++}i=0;
-if(u[1]){while(i<u.length-1){
+if(u[1])while(i<u.length-1){
 e=u[i+2];
 if(e>0)b[i/2].click();
 l(u[i+1]);
 i+=2}
-k[1]="a"}
 e=h[1].innerHTML}
 if(d[0].style.width==="100%")
 d[0].style.width="100px";
@@ -90,14 +84,11 @@ x=t.value;
 g=x.length;
 if(g==40){localStorage.setItem(0,x);z(u[0])}
 else if(g>0 && e!=0){
-n.open("GET",k[3]+"s/search?api_key="+k[0]+"&query="+x);
-n.send();
-n.onload=function(){
-j=JSON.parse(n.responseText);
+fetch(k[1]+"s/search?api_key="+k[0]+"&query="+x).then(i=>i.json()).then(j=>{
 i=0;d[4].innerHTML="<b><p>data type</p>food name ~ ingredients ~ brand owner</b>";
 while(i<50){
 d[4].innerHTML+="<b id='"+j.foods[i].fdcId+"' onclick='l(this.id);d[4].removeChild(this);'><p>"+j.foods[i].dataType+"</p>"+j.foods[i].description+" ~ "+j.foods[i].ingredients+" ~ "+j.foods[i].brandOwner+"</b>";
-i++}}}
+i++}})}
 v="for "+k[2]+" days:\n";
 g="";
 i=0;
